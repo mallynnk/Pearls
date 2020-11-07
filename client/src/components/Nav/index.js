7047c8619a35659c2338540ac002f97ee2000773
mallynnk <65303647+mallynnk@users.noreply.github.com>
2020-11-07 14:55:26 -0600

    Revert "Feature/css mel"

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,47 +3,42 @@ import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <section className="flex-row col">
-          <h2 className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
-          </h2>
-          <h2 className="mx-1">
-            <Link to="/shophome">
-              Shop
-            </Link>
-          </h2>
-          <h2 className="mx-1">
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">Order History</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/shophome">Shop</Link>
+          </li>
+          <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
-          </h2>
-        </section>
+          </li>
+        </ul>
       );
     } else {
       return (
-        <ul className="flex-row col">
-          <h2 className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </h2>
-          <h2 className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </h2>
-          <h2 className="mx-1">
-            <Link to="/shophome">
-              Shop
-            </Link>
-          </h2>
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/shophome">Shop</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/restaurants">Restaurants</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/faq">FAQ</Link>
+          </li>
         </ul>
       );
     }
@@ -51,15 +46,11 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
-      <h1 class="roy">
-        <Link to="/">
-          Roysters
-        </Link>
+      <h1>
+        <Link to="/">-Roysters</Link>
       </h1>
 
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
