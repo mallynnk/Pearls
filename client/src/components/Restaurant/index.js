f91aeb5a0282a36f79f16727af1e164d50fb86f2
mallynnk <mallory.korpics@gmail.com>
2020-11-09 10:32:00 -0600

    added restaurant and faq basic css

--- client/src/components/Restaurant/index.js
+++ client/src/components/Restaurant/index.js
@@ -14,33 +14,37 @@ function Restaurant() {
   
   return (
     <div>
-      <div>
-        <h1>● RALEIGH ●</h1>
-        <div>
+      <div class="restBox">
+        <h2 class="restHead">RALEIGH</h2>
+        <div class="restInfo">
         <h4>ST. ROCH FINE OYSTERS + BAR</h4>
         <p>223 SOUTH WILMINGTON STREET, RALEIGH, NC, 27601</p>
         <img src={stRoch} style={style} alt="st roch logo"></img>
         </div>
-        <div>
-        <h3>MADDEN'S SEAFOOD</h3>
+        </div>
+        <div class="restBox">
+        <div class="restInfo">
+        <h2 class="restHead">MADDEN'S SEAFOOD</h2>
         <p>8401 HONEYCUTT ROAD, RALEIGH, NC 27615</p>
         <img src={maddensSeafood} alt='maddens seafood logo'></img>
         </div>
       </div>
-      <div>
-        <h1>● BEAUFORT ●</h1>
-        <div>
+      <div class="restBox">
+        <h2 class="restHead">BEAUFORT</h2>
+        <div class="restInfo">
         <h3>ROSE SEAFOOD RETAIL AND WHOLESALE</h3>
         <p>1646 LIVE OAK STREET, BEAUFORT, NC 28516</p>
         <img src={roseSeafood} alt='rose seafood logo'></img>
         </div>
-        <div>
-        <h3>BLACKBEARD'S GRILL AND STEAM BAR</h3>
+        </div>
+        <div class="restBox">
+        <h2 class="restHead">BLACKBEARD'S GRILL AND STEAM BAR</h2>
+        <div class="restInfo">
         <p>1644 LIVE OAK STREET, BEAUFORT, NC 28516</p>
         <img src={blackbeard}  alt='blackbeardseafood logo'></img>
         </div>
+        </div>
       </div>
-    </div>
   );
 }
 
